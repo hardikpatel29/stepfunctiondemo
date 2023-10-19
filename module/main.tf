dc2e95a3a1f641c4e1c7ee4532a1e84e4bf074b1
PatelSaheb <patelsaheb29@gmail.com>
2023-10-19 11:49:56 +0530

    updated

--- module/main.tf
+++ module/main.tf
@@ -6,6 +6,17 @@ resource "aws_sfn_state_machine" "MyExample" {
   type = "EXPRESS"
   definition = data.template_file.my_state_machine.rendered
   depends_on = [ aws_lambda_function.my_lambda ] 
+  /* logging {
+    include_execution_data = true
+    level                 = "ALL"
+    destinations = [
+      {
+        cloud_watch_logs {
+          log_group_arn = aws_cloudwatch_log_group.stepfunctions_log_group.arn
+        }
+      }
+    ]
+  } */
 }
 
 data "template_file" "my_state_machine" {
